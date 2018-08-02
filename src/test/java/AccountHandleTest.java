30d4e11876d8db07bbcd21174e60628681dbbe0f
Yohan M <yohanmouyal@gmail.com>
2018-08-02 12:59:59 +0300

    Rien de special

---
+++ src/test/java/AccountHandleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.text.ParseException;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class AccountHandleTest
+{
+    @Test
+    public void shouldReturnZeroEmpruntWhenZeroEmprunt() throws ParseException {
+        assertEquals(0,0);
+    }
+}
